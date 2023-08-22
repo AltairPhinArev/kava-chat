e8124ff5f11f98f3c7318c3098ee902905ce43ab
Andrey Panasiuk <andreypnick@gmail.com>
2023-08-22 10:51:59 +0300

    intial commit

---
+++ chat/src/test/java/com/kava/vaadin/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kava.vaadin.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
